b02a4e40e95b78f5ceee91bcac726b648368802c
unknown <kurashin0729@gmail.com>
2023-08-06 01:08:25 +0900

    とりあえずチェックボックスの値が変わったらのところまで。何をするかはまだ書いてない。

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ $("#addTodo").click(function () {
 
 	$("input").val("");
 });
+
+$(document).on("change", "input[type=checkbox]", function() {
+	if($(this).is(":checked")) {
+		// チェックされた場合は右横に「タスク削除」のボタンを追加
+	} else {
+		// チェックが外れた場合は「タスク削除」のボタンを削除
+	};
+});
