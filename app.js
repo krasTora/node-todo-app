48a9e36105892d06fa7fe4285ceac4227b5bcf11
倉田真吾 <shingokurata@kuratashingonoMacBook-Pro.local>
2023-09-04 23:51:49 +0900

    今後は作成したhtmlファイルをブラウザに表示させるように実装する。

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-// ToDo: ここにURLから飛んできた時の処理を記述する
-console.log(`Hello World!!`);
+`use strict`;
+
+const express = require(`express`);
+const app = express();
+const port = 3000;
+
+app.get(`/`, (req, res) => {
+	return res.send(`Hello World!`);
+});
+
+app.listen(port, () => {
+	console.log(`ポート番号${port}番を利用してサーバーを起動しました`);
+});
