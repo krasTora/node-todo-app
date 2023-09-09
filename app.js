402b2906a18c71014733ace65e97d527d0db9bca
倉田真吾 <shingokurata@kuratashingonoMacBook-Pro.local>
2023-09-09 20:18:28 +0900

    画面項目に登録ボタンを追加、DBへ登録する処理以外は実装済

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 `use strict`;
 
 const express = require(`express`);
+const bodyParser = require(`body-parser`);
 const app = express();
 const port = 3000;
+// POSTで受信するときに必要らしい（よくわからん）。この二つを書かないとリクエストボディにundefinedが渡ってきてしまう。
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
+// 静的ファイルをクライアントへ返却
 app.use(express.static(`views`));
 
-app.get(`/`, (req, res) => {
-	return res.send(`Hello World!`);
+// 画面の登録ボタン押下時に渡ってくるリクエスト
+app.post(`/ajax/register/`, (req, res) => {
+	let reqData = JSON.parse(req.body.inputData);
+	// DBへ登録する処理を書く
+	res.json(reqData);
 });
 
 app.listen(port, () => {
-	console.log(`ポート番号${port}番を利用してサーバーを起動しました`);
+	console.log(`Start server port: ${port}`);
 });
--- views/index_cs.js
+++ views/index_cs.js
@@ -1,34 +1,59 @@
 `use strict`;
 
-// タスク追加ボタン押下時の処理
-$("#addTodo").click(() => {
-	const inputTodo = $("#inputArea").val();
-	if(!inputTodo) {
-		alert('雑魚が！！！');
-		return;
-	};
-	$("#todoList").append(`<li><input type='checkbox'>${inputTodo}</li>`);
-
-	$("#inputArea").val("");
-});
-
-// 登録ボタン押下時の処理
-$("#registButton").click(() => {
-	let inputDataArray = [];
-	$("#todoList li").each((i, el) => {
-		inputDataArray.push($(el).text());
+$(() => {
+	// タスク追加ボタン押下時の処理
+	$("#addTodo").click(() => {
+		const inputTodo = $("#inputArea").val();
+		if(!inputTodo) {
+			alert('雑魚が！！！');
+			return;
+		};
+		$("#todoList").append(`<li><input type='checkbox'>${inputTodo}</li>`);
+		// 入力エリアの値を削除する
+		$("#inputArea").val("");
 	});
-	// テキストを取得することはできたけど、タスクの完了状況も登録するようにしたいよね
-})
 
-$(document).on("change", "input[type=checkbox]", () => {
-	if($(this).is(":checked")) {
-		// 追加するボタンのID属性の値を採番
-		const buttonId = 'toriaezu';
-		// チェックされた場合は右横に「タスク削除」のボタンを追加
-		$(this).parent().append(`<input type='button' onclick='javaScript:$(this).parent().remove();' value='削除'>`);
-	} else {
-		// チェックが外れた場合は「タスク削除」のボタンを削除
-		$(this).parent().find('input[type=button]').remove();
-	};
-});
+	// チェックボックスの入力状態が変わった時の処理
+	// 備忘: functionをアロー関数に置き換えるとifの中に入ってこないなんで？（おそらくthisに入ってくる値が異なる、、、？）
+	$(document).on('change', 'input[type="checkbox"]', function () {
+		if($(this).is(":checked")) {
+			// チェックされた場合は右横に「タスク削除」のボタンを追加
+			$(this).parent().append(`<input type='button' onclick='javaScript:$(this).parent().remove();' value='削除'>`);
+		} else {
+			// チェックが外れた場合は「タスク削除」のボタンを削除
+			$(this).parent().find('input[type=button]').remove();
+		};
+	});
+
+	// 登録ボタン押下時の処理
+	$("#registerButton").click(() => {
+		// 項目名とチェックボックスの入力状況を取得
+		let inputDataArray = [];
+		$("#todoList li").each((i, el) => {
+			const itemObj = {
+				checkStatus: $(el).find(`input[type=checkbox]`).prop(`checked`),
+				itemName: $(el).text(),
+			};
+			inputDataArray.push(itemObj);
+		});
+
+		// AjaxのdataTypeに合わせるためjson文字列に変換
+		const jsonData = JSON.stringify(inputDataArray);
+
+		// 取得した情報をサーバサイドへ送信する
+		$.ajax({
+			type: "POST",
+			url: "http://localhost:3000/ajax/register",
+			dataType: "JSON",
+			data: {
+				inputData: jsonData,
+			},
+		})
+		.done((res) => {
+			console.log(`データの登録に成功しました。登録した情報は${ res.length }個です。`);
+		})
+		.fail((hr, status, error) => {
+			console.log(`サーバとの通信に失敗しました。`)
+		});
+	})
+})
